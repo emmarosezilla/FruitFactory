ea3f25f6cc51128bfc92f49ae4fdd15f3198c97e
emmarosezilla <emmarosezilla@gmail.com>
2023-11-02 11:24:14 -0400

    set parametres to fruit constructor and made a new mango & kiwi fruit.

--- src/Fruit.java
+++ src/Fruit.java
@@ -1,28 +1,34 @@
 public class Fruit {
 
     // variable declarations
-    public String colour;
+    public String color;
     public String shape;
     public int numSeeds;
     public boolean isRipe;
     public int numBush;
 
+    public Fruit (String paramColor, int paramNumSeeds,
+                  String paramShape, boolean paramisRipe) {
+        color = paramColor;
+        numSeeds = paramNumSeeds;
+        shape = paramShape;
+        numBush = (int)(Math.random()*100);
+        isRipe = paramisRipe;
+
+    }
+
     public void printInfo(){
-        System.out.println("The fruit's colour is " + colour);
-        System.out.println("The fruit is shaped like a(n) " + shape);
-        System.out.println("The fruit has " + numSeeds + " seed(s)!");
-        System.out.println("There are " + numBush + " fruits in this bush!");
+        System.out.println("This fruit's colour is " + color);
+        System.out.println("This fruit is shaped like a " + shape);
+        System.out.println("This fruit has " + numSeeds + " seed(s)!");
+        System.out.println("There are typically " + numBush + " fruits in a bush!");
         if (isRipe == true) {
-            System.out.println("The fruit is ripe.");
+            System.out.println("This particular fruit is ripe.");
         }
         else {
-            System.out.println("The fruit is not ripe. :(");
+            System.out.println("This particular fruit is not ripe. :(");
         }
     }
 
 
-    public Fruit (){
-
-
-    }
 }
--- src/FruitHQ.java
+++ src/FruitHQ.java
@@ -1,6 +1,6 @@
 public class FruitHQ {
     public static void main(String[] args) {
-        System.out.println("Welcome to the fruit factory!");
+        System.out.println("\n Welcome to the fruit factory! \n");
     FruitHQ myFactory = new FruitHQ();
 
     }
@@ -10,25 +10,25 @@ public class FruitHQ {
     // that has the same name as the class it's in
 
     public FruitHQ(){
-        // strawberry: red, triangle, 200, yes, 20
         Fruit strawberry;
-        strawberry = new Fruit ();
-        strawberry.colour = "red";
-        strawberry.shape = "triangle";
-        strawberry.numSeeds = 200;
-        strawberry.numBush = 20;
-        strawberry.isRipe = true;
+        strawberry = new Fruit ("red", 200,
+                "triangle", true);
 
         Fruit mango;
-        mango = new Fruit ();
-        mango.colour = "yellowish-orange";
-        mango.shape = "oval";
-        mango.numSeeds = 1;
-        mango.numBush = 100;
-        mango.isRipe = false;
+        mango = new Fruit ("yellowy orange, though it is sometimes green.", 1,
+                "kidney", false);
 
+        Fruit kiwi;
+        kiwi = new Fruit ("brown on the outside, green on the inside,", 60,
+                "sphere", true);
+
+        strawberry.printInfo();
+        System.out.println("Can you guess what kind of fruit I am? \n" );
 
         mango.printInfo();
+        System.out.println("Can you guess what kind of fruit I am?\n" );
+
+        kiwi.printInfo();
         System.out.println("Can you guess what kind of fruit I am?");
 
 
